dc66ab3aae0627c095dcdd478d69584f1a35aa3f
LaurieKD <124356856+LaurieKD@users.noreply.github.com>
2023-04-18 15:56:35 +0200

    Fixed a bug by deleting redundant code

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,38 +5,23 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
 	let [word, setWord] = useState(null);
-	let [searchWord, setSearchWord] = useState(false);
 	let [results, setResults] = useState(null);
-	let [phonetic, setPhonetic] = useState(null);
-	let [meaning, setMeaning] = useState(null);
 
-	function handleWordChange(event) {
-		event.preventDefault();
-		setSearchWord(false);
-		setWord(event.target.value);
-	}
-
-	function handleReset(event) {
-		event.preventDefault();
-		setSearchWord(false);
+	function showResults(response) {
+		setResults(response.data);
+		console.log(response.data);
 	}
 
 	function search(event) {
 		event.preventDefault();
-		setSearchWord(true);
 
 		let apiKey = "2335b3c82b10f0343t05f9bo28bfaca3";
 		let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
 		axios.get(apiUrl).then(showResults);
 	}
 
-	function showResults(response) {
-		setPhonetic(response.data.phonetic);
-		setMeaning(response.data.meanings[0].definition);
-		setResults(response.data);
-		console.log(response.data);
-		console.log(response.data.phonetic);
-		console.log(response.data.meanings[0].definition);
+	function handleWordChange(event) {
+		setWord(event.target.value);
 	}
 
 	let form = (
@@ -50,22 +35,19 @@ export default function Dictionary() {
 					placeholder="Search for a word"
 					className="Search-input"
 					onChange={handleWordChange}
-					onClick={handleReset}
 				/>
 			</form>
 		</div>
 	);
 
-	if (searchWord) {
+	if (results) {
 		return (
 			<div className="Dictionary">
 				{form}
 
-				<p>
+				<p className="You-searched">
 					You searched for the word <span className="Word">{word}</span>
 				</p>
-				<p>{meaning}</p>
-				<p>{phonetic}</p>
 				<Results results={results} />
 			</div>
 		);
--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,15 @@
 import React from "react";
+import "./Results.css";
 
 export default function Results(props) {
 	console.log(props.results.phonetic);
-	return <div className="Results">hello {props.results}</div>;
+	return (
+		<div className="Results">
+			<p className="Phonetic">/{props.results.phonetic}/</p>
+			<p className="What">{props.results.meanings[0].partOfSpeech}</p>
+			<p className="Meaning">{props.results.meanings[0].definition}</p>
+			<p className="What">Synonyms</p>
+			<p className="Example">{props.results.meanings[0].synonyms[0]}</p>
+		</div>
+	);
 }
